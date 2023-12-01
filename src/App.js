9c9f7ebb46b657aec7f3f613e0af4fa4c5d5fed4
beiyo-official <142246105+beiyo-official@users.noreply.github.com>
2023-12-02 01:52:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import arrowup from './images/icons8-arrow-up-50.png'
 function App() {
   return (
    <>
