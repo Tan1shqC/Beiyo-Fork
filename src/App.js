d2c81739c1ff7cd36d641060e97836ffbb41ab6f
beiyo-official <142246105+beiyo-official@users.noreply.github.com>
2023-12-02 03:56:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import arrowup from './images/icons8-arrow-up-50.png'
+
+import whatsapp from './images/WhatsApp.svg'
 function App() {
   return (
    <>
     <Navbar />
-    <Hero/>
-    <Footer/>
+    <div id="mySidenav" className="sidenav">
+
+<ul className='navbar-content-mobile'>
+
+<li  className='nav-item-mobile'><a href="">Home</a></li>
+<li  className='nav-item-mobile'><a href="">Subscribe</a></li>
+<li  className='nav-item-mobile'><a href="">Hostel</a></li>
+<li  className='nav-item-mobile'><a href="">List</a></li>
+
+</ul>
+<div className='whatsapp'>
+<a className='whatsapplink' href="https://wa.me/918305523140"><img src={whatsapp} alt="" />Chat with us</a>
+</div>
+</div>
+    <Hero  />
+    <Footer />
+
    </>
   );
 }
