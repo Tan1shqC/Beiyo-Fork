2ed93d7f4941f471caecab186160497160e3d395
DakshBaxi <111555273+DakshBaxi@users.noreply.github.com>
2023-12-26 20:27:44 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB (replace 'your_mongodb_uri' with your actual MongoDB URI)
+mongoose.connect('mongodb+srv://beiyowork:PjP0Jhpz0XCStQgY@beiyocustomers.0h5hffr.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define MongoDB Schema
+const formSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  message: String,
+  occupancy: String,
+});
+
+const Form = mongoose.model('Form', formSchema);
+
+// Handle form submissions
+app.post('/submit-form', async (req, res) => {
+  try {
+    const formData = req.body;
+    const newForm = new Form(formData);
+    await newForm.save();
+    res.status(200).json({ success: true, message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
